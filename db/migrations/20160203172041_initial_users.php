3376a68908f214ad716929b5b1030e98de0a0bb1
Vítězslav Dvořák <info@vitexsoftware.cz>
2022-09-03 22:44:26 +0200

    Phinx update

--- db/migrations/20160203172041_initial_users.php
+++ db/migrations/20160203172041_initial_users.php
@@ -10,12 +10,12 @@ class InitialUsers extends AbstractMigration
     public function change()
     {
         $userData = [
-            'email' => 'vitex@arachne.cz', 
+            'email' => 'info@vitexsoftware.cz', 
             'login' => 'vitex',
             'settings' => 'a:1:{s:5:"admin";s:4:"true";}',
             'password' => '7254a96290b564d1b0cd85b9881b6b1a:b3',
             'firstname' => 'Vítězslav',
-            'lastname' => 'Dvořák', 'DatCreate' => 'NOW()'];
+            'lastname' => 'Dvořák', 'DatCreate' => Envms\FluentPDO\Literal::from('NOW()')];
 
         $table = $this->table('user');
         $table->insert($userData);
